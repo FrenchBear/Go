c0a5ab8f5b0f89618cf4a9932ce4bff9b8e44582
French Bear <FrenchBear38@outlook.com>
2019-10-26 23:05:41 +0200

    Added test resp.StatusCode != http.StatusOK

--- The Go Programming Language/ch1/05_fetch1/fetch1.go
+++ The Go Programming Language/ch1/05_fetch1/fetch1.go
@@ -3,6 +3,7 @@
 // Simple get text from url(s) passed as parameter
 //
 // 2019-08-29	PV
+// 2019-10-26	PV		Added test resp.StatusCode != http.StatusOK
 
 package main
 
@@ -23,6 +24,11 @@ func main() {
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch1: error %v\n", err)
+			continue
+		}
+		if resp.StatusCode != http.StatusOK {
+			resp.Body.Close()
+			fmt.Fprintf(os.Stderr, "fetch1: error accessing %s: %s", url, resp.Status)
 			continue
 		}
 
