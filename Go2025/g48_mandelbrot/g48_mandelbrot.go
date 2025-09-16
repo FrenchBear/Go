4c2eeb3a1f810e414a27b3b3f4b0746d92150810
French Bear <frenchbear38@outlook.com>
2025-09-16 16:02:31 +0200

    Mandelbrot in Go, 2nd version

---
+++ Go2025/g48_mandelbrot/g48_mandelbrot.go
@@ -0,0 +1,58 @@
+// mandelbrot.go
+// Learning go, Generating a png image
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+//
+// 2019-10-07	PV		First version, from TGPL book
+// 2025-09-16	PV		Write directly a .png file instead of stdout
+
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+
+	f, err := os.Create("mandelbrot.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	if err := png.Encode(f, img); err != nil {
+		log.Fatalf("png.Encode failed: %v", err)
+	}
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}
---
+++ Go2025/g48_mandelbrot/go.mod
@@ -0,0 +1,3 @@
+module g48_mandelbrot
+
+go 1.25.1
