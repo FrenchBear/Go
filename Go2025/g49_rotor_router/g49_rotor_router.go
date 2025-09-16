2d280e9cba4c6b4cd2e406913d3258b0b1e9bfed
French Bear <frenchbear38@outlook.com>
2025-09-16 21:12:37 +0200

    rotor_router in Go

---
+++ Go2025/g49_rotor_router/g49_rotor_router.go
@@ -0,0 +1,72 @@
+// g49_rotor_router.go
+// Learning go, Generating a png image
+// From Complexités, JP Delahaye, P.232, 236
+//
+// 2025-09-16	PV		First version in Go
+
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	const (
+		r = 150
+		n = 70_000		// max π.r² iterations
+	)
+
+	col0 := color.RGBA{255, 0, 0, 255}
+	col1 := color.RGBA{0, 0, 255, 255}
+	col2 := color.RGBA{0, 255, 0, 255}
+	col3 := color.RGBA{0, 255, 255, 255}
+
+	width := 2*r+1
+	height := 2*r+1
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	start := time.Now()
+	for i := 0; i < n; i++ {
+		x:=r
+		y:=r
+Loop:
+		for {
+			switch img.At(x,y) {
+				case col0:
+					img.Set(x,y,col1)
+					y++
+				case col1:
+					img.Set(x,y,col2)
+					x++
+				case col2:
+					img.Set(x,y,col3)
+					y--
+				case col3:
+					img.Set(x,y,col0)
+					x--
+				default:
+					img.Set(x,y,col0)
+					break Loop
+			}
+		}
+	}
+	duration := time.Since(start)
+
+	f, err := os.Create("rotor_router.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	if err := png.Encode(f, img); err != nil {
+		log.Fatalf("png.Encode failed: %v", err)
+	}
+
+	fmt.Printf("Done for %d iterations in %.3fs\n", n, float64(duration.Milliseconds())/1000.0)
+}
---
+++ Go2025/g49_rotor_router/go.mod
@@ -0,0 +1,3 @@
+module g49_rotor_router
+
+go 1.25.1
